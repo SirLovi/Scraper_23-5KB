6eec1229cf0aa38e19faeb28152b6894c534b878
JiriWeiss <hafan002@gmail.com>
2021-11-09 08:37:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -32,10 +32,42 @@ async def main():
     await page.type('[id=passwordInput]', loginConfig.get('password'))
     await page.keyboard.press('Enter')
     await page.waitForNavigation()
-
-    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101010701')
     
-    #await page.screenshot({'path': 'outputs/example.png'})
+    urls = await page.evaluate('''
+        () => {
+            const links = document.querySelectorAll('ctl00_PlaceHolderMain_GridView1 a')
+            const urls = Array.from(links).map(link => link.href)
+            return urls
+        }
+    ''')
+    
+    print(urls)
+    await page.goto(urls[0])
+    """
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101010701')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101010703')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101011501')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101011601')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101011603')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101011701')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101011703')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101020701')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101020703')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101021501')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101021601')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101021701')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101021703')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101030701')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101030703')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101031501')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101031601')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101690701')
+    await page.goto('https://intranet.unob.cz/prehledy/SitePages/stskupiny2.aspx?hr=9101690703')
+    """
+    
+   
+    
+    await page.screenshot({'path': 'outputs/example.png'})
     
     await browser.close()
 
